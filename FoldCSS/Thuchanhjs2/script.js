d949eedac972912b1ddf0f35a4a75b26b1b37be5
Vu Thanh Hung <hung.thanhvu24@gmail.com>
2025-05-27 02:02:02 +0000

    up

--- FoldCSS/Thuchanhjs2/script.js
+++ FoldCSS/Thuchanhjs2/script.js
@@ -1,3 +1,9 @@
+
+const defacar = "photo/pt1.jpg"
+
 function changeCar(imagePath) {
   document.getElementById("mainCar").src = imagePath;
+}
+function defc() {
+    document.getElementById("mainCar").src = defacar;
 }
